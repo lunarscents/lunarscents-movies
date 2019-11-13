2af93efd56577b71418264431e21e61e80e32364
lunarscents <lunarscents7@gmail.com>
2019-11-13 22:46:13 +0900

    [#2.0] - add constants and created a tab navigation

--- App.js
+++ App.js
@@ -1,19 +1,34 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { AppLoading, Font, Asset } from "expo";
+import { Ionicons } from "@expo/vector-icons";
+import TabNavigation from "./navigation/TabNavigation";
 
 export default function App() {
-  return (
+  state = {
+    loaded: false
+  };
+
+  handleError = () => console.log(error);
+
+  handleLoaded = () => this.setState({ loaded: true });
+
+  loadAssets = async () => {
+    await Font.loadAsync({
+      ...Ionicons.font
+    });
+
+    await Asset.loadAsync([require("images/icon.png")]);
+  };
+
+  return loaded ? (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TabNavigation />
     </View>
+  ) : (
+    <AppLoading
+      startAsync={this.loadAssets}
+      onFinish={this.handleLoaded}
+      onError={this.handleLoaded}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/Search.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+import { TEXT } from "react-native";
+
+export default () => <TEXT>SEARCH</TEXT>;
