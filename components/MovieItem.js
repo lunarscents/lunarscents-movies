17839af5650a5560441a313c0249e67e7ecb4fe0
lunarscents <lunarscents7@gmail.com>
2019-11-20 22:41:32 +0900

    [#6.1] - Horizontal Movies Part Two

--- components/MovieItem.js
+++ components/MovieItem.js
@@ -3,14 +3,21 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 import MovieRating from "./MovieRating";
 
-const Container = styled.View``;
+const Container = styled.View`
+  align-items: center;
+  margin-right: 20px;
+`;
 
-const Title = styled.Text``;
+const Title = styled.Text`
+  color: white;
+  font-size: 12px;
+  margin-vertical: 5px;
+`;
 
 const MovieItem = ({ id, posterPhoto, title, voteArg }) => (
   <Container>
     <MoviePoster path={posterPhoto} />
-    <Title>{title}</Title>
+    <Title>{title.length > 20 ? `${title.substring(0, 12)}...` : title}</Title>
     <MovieRating votes={voteArg} />
   </Container>
 );
