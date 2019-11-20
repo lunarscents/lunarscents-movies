261b8cf23745ff2a4b3882f260a976d0529b6ad6
lunarscents <lunarscents7@gmail.com>
2019-11-20 23:03:24 +0900

    [#6.3] - Vertical Movies Part Two

--- components/MovieItem.js
+++ components/MovieItem.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import MovieRating from "./MovieRating";
+import { GREY_COLOR } from "../constants/Colors";
 
 const Container = styled.View`
   align-items: center;
@@ -10,16 +11,54 @@ const Container = styled.View`
 
 const Title = styled.Text`
   color: white;
-  font-size: 12px;
+  font-size: ${props => (props.big ? "12px" : "14px")};
   margin-vertical: 5px;
 `;
 
-const MovieItem = ({ id, posterPhoto, title, voteArg, horizontal = false }) =>
-  horizontal ? null : (
+const HContainer = styled.div`
+  margin-bottom: 20px;
+  flex-direction: row;
+`;
+
+const Column = styled.View`
+  margin-left: 20px;
+  width: 60%;
+`;
+
+const Overview = styled.Text`
+  color: ${GREY_COLOR};
+  font-size: 12px;
+  margin-vertical: 10px;
+`;
+
+const MovieItem = ({
+  id,
+  posterPhoto,
+  title,
+  voteArg,
+  horizontal = false,
+  overview
+}) =>
+  horizontal ? (
+    <HContainer>
+      <MoviePoster path={posterPhoto} />
+      <Column>
+        <Title>{title}</Title>
+        <MovieRating votes={voteArg} />
+        {overview ? (
+          <Overview>
+            {overview.length > 117
+              ? `${overview.substring(0, 120)}...`
+              : overview}
+          </Overview>
+        ) : null}
+      </Column>
+    </HContainer>
+  ) : (
     <Container>
       <MoviePoster path={posterPhoto} />
       <Title>
-        {title.length > 20 ? `${title.substring(0, 12)}...` : title}
+        {title.length > 15 ? `${title.substring(0, 12)}...` : title}
       </Title>
       <MovieRating votes={voteArg} />
     </Container>
@@ -29,7 +68,8 @@ MovieItem.propTypes = {
   id: PropTypes.number.isRequired,
   posterPhoto: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  voteArg: PropTypes.number.isRequired
+  voteArg: PropTypes.number.isRequired,
+  overview: PropTypes.string
 };
 
 export default MovieItem;
