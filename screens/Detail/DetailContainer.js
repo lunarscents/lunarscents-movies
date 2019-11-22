ccdf6d49f5ea9fb0028066c01f784264d9cf385e
lunarscents <lunarscents7@gmail.com>
2019-11-22 11:54:34 +0900

    [#8.4] - WTF Bug

--- screens/Detail/DetailContainer.js
+++ screens/Detail/DetailContainer.js
@@ -1,6 +1,6 @@
 import React from "react";
-import PropTypes from "prop-types";
 import DetailPresenter from "./DetailPresenter";
+import { movies } from "../../api";
 
 export default class extends React.Component {
   static navigationOptions = ({ navigation }) => {
@@ -39,11 +39,40 @@ export default class extends React.Component {
   }
 
   async componentDidMount() {
-    let error, genres, overview;
+    const { isMovie, id } = this.state;
+    let error, genres, overview, status, date, backgroundPhoto;
     try {
-    } catch {
+      if (isMovie) {
+        ({
+          genres,
+          overview,
+          status,
+          release_date: date,
+          backdrop_path: backgroundPhoto
+        } = await movies.getMovie(id));
+      } else {
+        ({
+          genres,
+          overview,
+          status,
+          release_date: date,
+          backdrop_path: backgroundPhoto
+        } = await tv.getShow(id));
+      }
+    } catch (error) {
+      console.log(error);
     } finally {
-      this.setState({ loading: false });
+      this.setState(prev => {
+        return {
+          ...prev,
+          loading: false,
+          genres,
+          overview,
+          status,
+          date,
+          backgroundPhoto
+        };
+      });
     }
   }
 
